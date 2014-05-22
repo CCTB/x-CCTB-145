5c493a9a478eac948bb2e5fd8e35a361af89a015
Dan Gilleland <dgilleland@nait.ca>
2014-05-22 10:42:48 -0600

    Chapter 3 - Flow Control + Enumeration Sample

--- Chapter 3/programflow1/Program.cs
+++ Chapter 3/programflow1/Program.cs
@@ -14,9 +14,87 @@ namespace programflow1
             Program myApp = new Program(); // Create an object based on this class
 
             myApp.IfStatementSample(); // Call (i.e.: execute) the method
+            myApp.WhileStatementSamples();
+            myApp.ForStatementSample();
+            myApp.ListDivisibleBy(3);
+            int someNumber;
+            Console.Write("Enter a small number between 2 and 20:");
+            someNumber = int.Parse(Console.ReadLine());
+            myApp.ListDivisibleBy(someNumber);
+            myApp.EnumSample(Color.Green); // pass in a literal value from my enum
 
             Console.WriteLine("Goodbye.");
+            Console.WriteLine("Press [enter] to exit...");
+            Console.ReadLine();
         } // end of the Main() method
+
+        private void EnumSample(Color favoriteColor)
+        {
+            Console.WriteLine("Sample Enumeration...");
+            
+            switch (favoriteColor)
+            {
+                case Color.Red:
+                    Console.WriteLine("Your color is red.");
+                    break;
+                case Color.Blue:
+                    Console.WriteLine("Your color is blue.");
+                    break;
+                case Color.Green:
+                    Console.WriteLine("Your color is green.");
+                    break;
+                default:
+                    Console.WriteLine("You entered a different color:");
+                    Console.WriteLine(favoriteColor);
+                    break;
+            }
+        }
+
+        private void ForStatementSample()
+        {
+            Console.WriteLine("For Sample...");
+            for (int counter = 1; counter <= 10; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+
+        private void ListDivisibleBy(int divisor)
+        {
+            Console.WriteLine("List all the numbers from 1 to 100 that are divisible by " + divisor);
+
+            for (int counter = 1; counter <= 100; counter++)
+            {
+                if (counter % divisor == 0)
+                {
+                    Console.Write(counter);
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine(); // write a line-feed
+        } // end of ListDivisibleBy() method
+
+        private void WhileStatementSamples()
+        {
+            Console.WriteLine("While and Do-While Samples...");
+
+            int counter = 10;
+            while (counter > 0)
+            { // we only enter the loop while the above condition is true
+                Console.WriteLine(counter);
+                counter = counter - 1;
+            }
+
+            int age;
+            do
+            {
+                Console.WriteLine("Enter your age once more: ");
+                age = int.Parse(Console.ReadLine());
+            } while (age < 1 || age > 100);
+            string message;
+            message = string.Format("You are {0} years old!", age);
+            Console.WriteLine(message);
+        } // end of the WhileStatementSamples() method
 
         public void IfStatementSample()
         {
@@ -26,7 +104,11 @@ namespace programflow1
             string stringAge = Console.ReadLine(); // Get the text the user types in
             int age = int.Parse(stringAge); // Convert the typed number to an int
 
-            if (age >= 18)
+            if (age >= 65)
+            {
+                Console.WriteLine("You are a senior.");
+            }
+            else if (age >= 18)
             {
                 Console.WriteLine("You are an adult.");
             }
